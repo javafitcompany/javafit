b0d9edaa5095d8570dab49fd0736c7c5f791a0a2
mtruong2004 <159853982+mtruong2004@users.noreply.github.com>
2024-04-25 16:25:18 -0400

    Update Users.java

--- Users.java
+++ Users.java
@@ -1,2 +1,29 @@
-public class Users {
+import java.util.Scanner;
+
+public class User {
+    private String name;
+    private int age;
+    private double weight;
+
+    public User(Scanner scanner) {
+        System.out.println("Enter your name:");
+        this.name = scanner.nextLine();
+        System.out.println("Enter your age:");
+        this.age = scanner.nextInt();
+        System.out.println("Enter your weight:");
+        this.weight = scanner.nextDouble();
+        scanner.nextLine();     }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
