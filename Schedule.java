c94801b0aa5bde5e0ec3cdd0ba23210b754a3a4e
Phantom <phantomanalytical@gmail.com>
2024-05-12 14:32:21 -0400

    Added Schedule code

--- Schedule.java
+++ Schedule.java
@@ -1,2 +1,36 @@
+import java.util.Date;
+
+/**
+ * Schedule class to manage workout dates using an array.
+ */
 public class Schedule {
+    private Date workoutDate;
+    private Workout[] exercises;
+    private int reps;
+
+    public Schedule(Date workoutDate) {
+        this.workoutDate = workoutDate;
+        this.exercises = new Workout[10];
+        this.reps = 0;
+    }
+
+    public void addWorkout(Workout workout) {
+        if (reps < exercises.length) {
+            exercises[reps++] = workout;
+        } else {
+            System.out.println("Maximum workouts reached for this schedule.");
+        }
+    }
+
+    public Workout[] getWorkouts() {
+        return exercises;
+    }
+
+    public Date getWorkoutDate() {
+        return workoutDate;
+    }
+
+    public int getWorkoutCount() {
+        return reps;
+    }
 }
